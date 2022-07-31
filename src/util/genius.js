56b5ef2789b9e834edf8187025253906fe587c2e
Alex Rosales <rosalesa512@gmail.com>
2022-07-31 04:04:57 -0700

    Moving to main computer

---
+++ src/util/genius.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+const dotenv = require('dotenv');
+
+dotenv.config()
+
+const option = {
+    API
+}
+
+module.exports = { 
+    searchForSong(searchTerm) {
+
+    }
+}
